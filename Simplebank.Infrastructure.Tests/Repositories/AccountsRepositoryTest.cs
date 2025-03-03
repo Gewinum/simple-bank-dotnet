3e96015deb348a450c9e001bbce07f096473b8f4
Nicholas Winterhalter <78866820+Gewinum@users.noreply.github.com>
2025-03-03 17:23:59 +0400

    test(infrastructure): add tests for accounts

---
+++ Simplebank.Infrastructure.Tests/Repositories/AccountsRepositoryTest.cs
@@ -0,0 +1,113 @@
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Moq.EntityFrameworkCore;
+using Simplebank.Domain.Constants;
+using Simplebank.Domain.Database.Models;
+using Simplebank.Infrastructure.Database;
+using Simplebank.Infrastructure.Exceptions;
+using Simplebank.Infrastructure.Repositories;
+
+namespace Simplebank.Infrastructure.Tests.Repositories;
+
+public class AccountsRepositoryTest
+{
+    [Fact]
+    public async Task GetAccountByIdAsyncSuccess()
+    {
+        // Arrange
+        var context = InstantiateContext();
+        var account = CreateRandomAccount();
+        
+        await context.AddAsync(account);
+        await context.SaveChangesAsync();
+
+        var repository = new AccountsRepository(context);
+
+        // Act
+        var result = await repository.GetByIdAsync(account.Id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(account.Id, result.Id);
+        Assert.Equal(account.Currency, result.Currency);
+        Assert.Equal(account.Balance, result.Balance);
+    }
+    
+    [Fact]
+    public async Task GetAccountByIdAsyncNotFound()
+    {
+        // Arrange
+        var context = InstantiateContext();
+        var repository = new AccountsRepository(context);
+
+        // Act
+        var result = await repository.GetByIdAsync(Guid.NewGuid());
+
+        // Assert
+        Assert.Null(result);
+    }
+    
+    [Fact]
+    public async Task AddAccountAsyncSuccess()
+    {
+        // Arrange
+        var context = InstantiateContext();
+        var account = CreateRandomAccount();
+        var repository = new AccountsRepository(context);
+
+        // Act
+        await repository.AddAsync(account);
+
+        // Assert
+        var result = await repository.GetByIdAsync(account.Id);
+        Assert.NotNull(result);
+        Assert.Equal(account.Id, result.Id);
+        Assert.Equal(account.Currency, result.Currency);
+        Assert.Equal(account.Balance, result.Balance);
+    }
+    
+    [Fact]
+    public async Task AddAccountAsyncDuplicateKeysException()
+    {
+        // Arrange
+        var context = InstantiateContext();
+        var account = CreateRandomAccount();
+        var repository = new AccountsRepository(context);
+        
+        await repository.AddAsync(account);
+        await context.SaveChangesAsync();
+
+        // Act
+        var exception = await Assert.ThrowsAsync<ArgumentException>(() => repository.AddAsync(account));
+        
+        // Assert
+        Assert.Equal(exception.Message, $"An item with the same key has already been added. Key: {account.Id}");
+    }
+    
+    private ApplicationDbContext InstantiateContext()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestCase")
+            .Options;
+        return new ApplicationDbContext(options);
+    }
+    
+    private Account CreateRandomAccount()
+    {
+        var randomCurrency = CurrencyConstants.Currencies[new Random().Next(CurrencyConstants.Currencies.Length)];
+        return new Account
+        {
+            Id = Guid.NewGuid(),
+            Owner = RandomString(10),
+            Currency = randomCurrency,
+            Balance = new Random().Next(0, 1000)
+        };
+    }
+
+    private string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[new Random().Next(s.Length)]).ToArray());
+    }
+}
